8199227b50c0a2097f4383d0727eea94a6f1c72c
kosinaz <kosinaz@gmail.com>
2022-05-04 23:33:55 +0200

    add firebase

--- src/menuScene.js
+++ src/menuScene.js
@@ -1,7 +1,11 @@
 import Phaser from './phaser.js';
+import Firebase from './firebase.js';
 
 export default class MenuScene extends Phaser.Scene {
-  create() {
-    console.log('menu');
+  async create() {
+    //await Firebase.setMap({'1,1': 'skull'});
+    await Firebase.updateMap({'1,2': 'skull'});
+    const map1 = await Firebase.getMap('map1');
+    console.log(map1.data());
   }
 }
