d7b1c56385b7d58b8a04116e233673ccc6dead6f
kosinaz <kosinaz@gmail.com>
2022-05-05 21:17:54 +0200

    add auth ui

--- src/menuScene.js
+++ src/menuScene.js
@@ -1,12 +1,27 @@
 import Phaser from './phaser.js';
 import { firebaseApp } from './firebase.js';
-import { getFirestore, doc, getDoc, updateDoc } from 'https://www.gstatic.com/firebasejs/9.7.0/firebase-firestore.js';
+import { getFirestore, doc, getDoc, setDoc } from 'https://www.gstatic.com/firebasejs/9.7.0/firebase-firestore.js';
+import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.7.0/firebase-auth.js';
 
 export default class MenuScene extends Phaser.Scene {
   async create() {
     const db = getFirestore(firebaseApp);
-    await updateDoc(doc(db, 'maps', 'map1'), {'4,4': 'skull'});
-    const map1 = await getDoc(doc(db, 'maps', 'map1'));
-    console.log(map1.data());
+    const auth = getAuth(firebaseApp);
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const uid = user.uid;
+        let progress = 0;
+        const userDoc = await getDoc(doc(db, 'users', uid));
+        const userData = userDoc.data();
+        if (userData) {
+          progress = userData.progress;
+        } else {
+          await setDoc(doc(db, 'users', uid), {'progress': progress});
+        }
+        const mapDoc = await getDoc(doc(db, 'maps', progress.toString()));
+        const map = mapDoc.data()
+        console.log(map);
+      }
+    });
   }
 }
